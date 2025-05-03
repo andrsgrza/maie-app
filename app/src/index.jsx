b5792c16fcbca34809f8517d110ed691a0b2bec9
andrsgrza <andresgarza1998@gmail.com>
2025-05-03 10:20:23 -0600

    Quixk fixes

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -4,16 +4,6 @@ import "./index.css";
 import MainPage from "./components/main-page/main-page.jsx";
 import { AuthProvider } from "./context/AuthContext";
 
-const ScrollToTop = () => {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-};
-
 class Maie extends React.Component {
   render() {
     return (
